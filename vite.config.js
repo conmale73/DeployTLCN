556c3e35bf2023a4560eaddad802dfddeeadf9e0
duchau <duchauvu0703@gmail.com>
2022-12-23 18:57:26 +0700

    fix1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
-import path from 'path'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), legacy()],
-  resolve: {
-    alias: {'~' : path.resolve(__dirname, './src')},
-  },
-})
+    plugins: [react(), legacy()],
+    resolve: {
+        alias: { "~": path.resolve(__dirname, "./src") },
+    },
+});
